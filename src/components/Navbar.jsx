cfe193836cab59ac0c826eca634ac2df8669127a
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-03-09 04:25:52 -0800

    add prevScrollPos and visible useState in Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,20 +5,22 @@
 import React, { useState } from 'react';
 
 const Navbar = () => {
+    const [prevScrollPos, setPrevScrollPos] = useState(0);
+    const [visible, setVisible] = useState(true);
 
-  const navbarStyles = {
-    position: 'fixed',
-    height: '60px',
-    width: '100%',
-    backgroundColor: 'grey',
-    textAlign: 'center'
-  }
+    const navbarStyles = {
+        position: 'fixed',
+        height: '60px',
+        width: '100%',
+        backgroundColor: 'grey',
+        textAlign: 'center'
+    }
 
-  return (
-    <div style={{ ...navbarStyles }}>
-      Some Company Inc.
-    </div>
-  );
+    return (
+        <div style={{ ...navbarStyles }}>
+            Some Company Inc.
+        </div>
+    );
 };
 
 export default Navbar;
