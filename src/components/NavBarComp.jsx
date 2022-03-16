e0c6eb39fa910903cf5cf08fe593a247fb0cd26d
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-03-16 16:59:06 -0700

    add aboutme hash ref to navbarcomp

--- src/components/NavBarComp.jsx
+++ src/components/NavBarComp.jsx
@@ -17,14 +17,14 @@ const NavBarComp = () => {
             <div className='navbar navbar-dark bg-navy-gradient box-shadow'>
                 <div className='container d-flex justify-content-between'>
                     {/* **** Site Title ******** */}
-                    <Link className='nav-title btn nav-btn-bg-clear' to="/">
+                    <a href='#title' className='nav-title btn nav-btn-bg-clear'>
                         <h2><strong><span className='text-yellow'>V</span>Cabading</strong></h2>
-                    </Link>
+                    </a>
                     {/* **** Link to Views ******** */}
                     <div className='row nav-btns align-items-center'>
-                        <Link to="/aboutme" className='col btn nav-btn-bg-clear' >
+                        <a href='#aboutme' className='col btn nav-btn-bg-clear' >
                             <strong>About Me</strong>
-                        </Link>
+                        </a>
                         <Link to="/projects" className='col btn nav-btn-bg-clear' >
                             <strong>Projects</strong>
                         </Link>
